c795876e2e5cb9201973f9a3ebe7bd3f8cee70ee
imadtg <i.haidi@outlook.com>
2024-07-12 00:26:22 +0100

    Wire up CircularColorsDemo

--- src/components/CircularColorsDemo/CircularColorsDemo.js
+++ src/components/CircularColorsDemo/CircularColorsDemo.js
@@ -1,62 +1,91 @@
-import React from 'react';
-import clsx from 'clsx';
-import {
-  Play,
-  Pause,
-  RotateCcw,
-} from 'react-feather';
+"use client";
+import React from "react";
+import clsx from "clsx";
+import { Play, Pause, RotateCcw } from "react-feather";
+import { motion } from "framer-motion";
 
-import Card from '@/components/Card';
-import VisuallyHidden from '@/components/VisuallyHidden';
+import Card from "@/components/Card";
+import VisuallyHidden from "@/components/VisuallyHidden";
 
-import styles from './CircularColorsDemo.module.css';
+import styles from "./CircularColorsDemo.module.css";
 
 const COLORS = [
-  { label: 'red', value: 'hsl(348deg 100% 60%)' },
-  { label: 'yellow', value: 'hsl(50deg 100% 55%)' },
-  { label: 'blue', value: 'hsl(235deg 100% 65%)' },
+  { label: "red", value: "hsl(348deg 100% 60%)" },
+  { label: "yellow", value: "hsl(50deg 100% 55%)" },
+  { label: "blue", value: "hsl(235deg 100% 65%)" },
 ];
 
-function CircularColorsDemo() {
-  // TODO: This value should increase by 1 every second:
-  const timeElapsed = 0;
+function useTimeElapsed(
+  initiallyPaused = true,
+  initialValue = 0,
+  intervalLength = 1000
+) {
+  const [timeElapsed, setTimeElapsed] = React.useState(initialValue);
+  const [isPaused, setIsPaused] = React.useState(initiallyPaused);
+  React.useEffect(() => {
+    if (isPaused) {
+      return;
+    }
+    const intervalId = window.setInterval(
+      () => setTimeElapsed((previousTimeElapsed) => previousTimeElapsed + 1),
+      intervalLength
+    );
 
-  // TODO: This value should cycle through the colors in the
-  // COLORS array:
-  const selectedColor = COLORS[0];
+    return () => {
+      window.clearInterval(intervalId);
+    };
+  }, [intervalLength, isPaused]);
+  function resetTimeElapsed(pauseAfterReset = initiallyPaused) {
+    setTimeElapsed(initialValue);
+    if (pauseAfterReset) {
+      setIsPaused(true);
+    }
+  }
+  function toggleTimeLapse() {
+    setIsPaused(!isPaused);
+  }
+  return {
+    timeElapsed,
+    isPaused,
+    resetTimeElapsed,
+    toggleTimeLapse,
+    setIsPaused,
+  };
+}
+
+function CircularColorsDemo() {
+  const { timeElapsed, isPaused, resetTimeElapsed, toggleTimeLapse } =
+    useTimeElapsed();
+  const id = React.useId();
+
+  const selectedColorIndex = timeElapsed % COLORS.length;
+
+  const selectedColor = COLORS[selectedColorIndex];
 
   return (
     <Card as="section" className={styles.wrapper}>
       <ul className={styles.colorsWrapper}>
         {COLORS.map((color, index) => {
-          const isSelected =
-            color.value === selectedColor.value;
+          const isSelected = color.value === selectedColor.value;
 
           return (
-            <li
-              className={styles.color}
-              key={index}
-            >
+            <li className={styles.color} key={index}>
               {isSelected && (
-                <div
-                  className={
-                    styles.selectedColorOutline
-                  }
+                <motion.div
+                  layoutId={`${id}-color-outline`}
+                  className={styles.selectedColorOutline}
                 />
               )}
               <div
                 className={clsx(
                   styles.colorBox,
-                  isSelected &&
-                    styles.selectedColorBox
+                  isSelected && styles.selectedColorBox
                 )}
                 style={{
                   backgroundColor: color.value,
                 }}
               >
-                <VisuallyHidden>
-                  {color.label}
-                </VisuallyHidden>
+                <VisuallyHidden>{color.label}</VisuallyHidden>
               </div>
             </li>
           );
@@ -69,11 +98,20 @@ function CircularColorsDemo() {
           <dd>{timeElapsed}</dd>
         </dl>
         <div className={styles.actions}>
-          <button>
-            <Play />
-            <VisuallyHidden>Play</VisuallyHidden>
+          <button onClick={toggleTimeLapse}>
+            {isPaused ? (
+              <>
+                <Play />
+                <VisuallyHidden>Play</VisuallyHidden>
+              </>
+            ) : (
+              <>
+                <Pause />
+                <VisuallyHidden>Pause</VisuallyHidden>
+              </>
+            )}
           </button>
-          <button>
+          <button onClick={resetTimeElapsed}>
             <RotateCcw />
             <VisuallyHidden>Reset</VisuallyHidden>
           </button>
