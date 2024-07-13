14a977b91a7aeff45fd100dabd8121b004047a4d
imadtg <i.haidi@outlook.com>
2024-07-13 23:09:31 +0100

    Make domain name dynamically retrieved

--- src/app/rss.xml/route.js
+++ src/app/rss.xml/route.js
@@ -1,26 +1,29 @@
 import RSS from "rss";
-import { BLOG_TITLE, BLOG_DESCRIPTION, DOMAIN, AUTHOR } from "@/constants.js";
+import { BLOG_TITLE, BLOG_DESCRIPTION, AUTHOR } from "@/constants.js";
 import { getBlogPostList } from "@/helpers/file-helpers.js";
+import { headers } from "next/headers";
 
 export async function GET() {
+  const headersList = headers();
+  const domain = headersList.get("host");
   const feed = new RSS({
     title: BLOG_TITLE,
     description: BLOG_DESCRIPTION,
-    site_url: DOMAIN,
-    feed_url: `${DOMAIN}/rss.xml`,
+    site_url: domain,
+    feed_url: `${domain}/rss.xml`,
   });
   const posts = await getBlogPostList();
   posts.forEach(({ slug, title, abstract, publishedOn }) =>
     feed.item({
       title,
       description: abstract,
-      url: `${DOMAIN}/${slug}`,
-      guid: `${DOMAIN}/${slug}`,
+      url: `${domain}/${slug}`,
+      guid: `${domain}/${slug}`,
       author: AUTHOR,
       date: publishedOn,
     })
   );
   const xml = feed.xml({ indent: true });
 
-  return new Response(xml, {headers: {"Content-Type": "application/xml"}});
+  return new Response(xml, { headers: { "Content-Type": "application/xml" } });
 }
--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,5 @@
 export const BLOG_TITLE = 'Bits & Bytes';
 export const BLOG_DESCRIPTION = "A wonderful blog about JavaScript";
-export const DOMAIN = "localhost"
 export const AUTHOR = "Josh W. Comeau"
 
 export const LIGHT_COLORS = {
